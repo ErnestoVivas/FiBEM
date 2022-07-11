45e7087edea6574d8b9d72a4bd888ab16d22a88f
nicarao.ernesto.walter <nicarao.ernesto.walter@rwth-aachen.de>
2022-07-11 23:28:04 +0200

    Test device push

--- src/fiware_config_dialog.py
+++ src/fiware_config_dialog.py
@@ -54,10 +54,10 @@ class FiwareConfigDialog(QtWidgets.QDialog):
                     client.post_entity(entity = ContextEntity(**(entity.base_attributes)))
 
             # post devices
-            with IoTAClient(url = fw_url + ":4041", fiware_header = fw_header) as iota_client:
-                for entity in self.building_energy_system.entities:
-                    for device in entity.devices:
-                        iota_client.post_device(device = Device(**device))
+            #with IoTAClient(url = fw_url + ":4041", fiware_header = fw_header) as iota_client:
+            #    for entity in self.building_energy_system.entities:
+            #        for device in entity.devices:
+            #            iota_client.post_device(device = Device(**device))
 
             post_successful_msg = QMessageBox()
             post_successful_msg.setIcon(QMessageBox.Information)
